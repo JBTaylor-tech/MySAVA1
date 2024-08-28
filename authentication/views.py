18c9d5e3161de4a80e47bc870602c107f60383df
Jemimah Bayode-Taylor <jemimah.bayodetaylor@gmail.com>
2024-08-28 16:23:24 +0100

    Add files via upload

--- authentication/views.py
+++ authentication/views.py
@@ -1,137 +1,95 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.contrib.auth.models import User
-from django.contrib import messages
-from django.core.mail import EmailMessage, send_mail, get_connection
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import JsonResponse
+from .models import Chat, Message
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+import os
+import random
+import nltk
+import string
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 from django.conf import settings
-from MySAVA import settings
-from django.contrib.sites.shortcuts import get_current_site
-from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
-from django.utils.encoding import force_bytes, force_str
-from django.contrib.auth import authenticate, login, logout
-from . tokens import generate_token
+from authentication.models import User
+from .nlp_model import *
 
-# Create your views here.
+
+@login_required
 def home(request):
-    return render(request, "authentication/index.html")
+    chats = Chat.objects.filter(user=request.user).order_by('-created')
+    return render(request, "chat/index.html", {'chats': chats})
 
-# Create your views here.
-def success(request):
-    return render(request, "authentication/success.html")
+def welcome(request):
+    return render(request, "auth/gs.html")
 
-
-# authentication/views.py
-
-from django.core.mail import EmailMessage, send_mail, get_connection
-from django.conf import settings
-
-def signup(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        fname = request.POST['fname']
-        lname = request.POST['lname']
-        email = request.POST['email']
-        pass1 = request.POST['pass1']
-        pass2 = request.POST['pass2']
-        
-        if User.objects.filter(username=username):
-            messages.error(request, "Username already exist! Please try some other username.")
-            return redirect('home')
-        
-        if User.objects.filter(email=email).exists():
-            messages.error(request, "Email Already Registered!!")
-            return redirect('home')
-        
-        if len(username)>20:
-            messages.error(request, "Username must be under 20 characters!!")
-            return redirect('home')
-        
-        if pass1 != pass2:
-            messages.error(request, "Passwords didn't match!!")
-            return redirect('home')
-        
-        if not username.isalnum():
-            messages.error(request, "Username must be Alpha-Numeric!!")
-            return redirect('home')
-        
-        myuser = User.objects.create_user(username, email, pass1)
-        myuser.first_name = fname
-        myuser.last_name = lname
-        myuser.is_active = False
-        myuser.save()
-        messages.success(request, "Your Account has been created successfully!! Please check your email to confirm your email address in order to activate your account.")
-        
-        # Welcome Email
-        subject = "Welcome to MySAVA Login!!"
-        message = f"Hello {myuser.first_name}!! \nWelcome to MySAVA!! \nThank you for visiting our website. We have also sent you a confirmation email, please confirm your email address.\n\nThank You\nJemimah Bayode-Taylor"        
-        from_email = settings.EMAIL_HOST_USER
-        to_list = [myuser.email]
-        
-        try:
-            send_mail(subject, message, from_email, to_list, fail_silently=False)
-        except Exception as e:
-            messages.error(request, f"Error sending welcome email: {e}")
-        
-        # Email Address Confirmation Email
-        current_site = get_current_site(request)
-        email_subject = "Confirm your Email @ MySAVA Login!!"
-        message2 = render_to_string('email_confirmation.html', {
-            'name': myuser.first_name,
-            'domain': current_site.domain,
-            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
-            'token': generate_token.make_token(myuser)
-        })
-        email = EmailMessage(email_subject, message2, from_email, to_list)
-        
-        try:
-            email.send(fail_silently=False)
-        except Exception as e:
-            messages.error(request, f"Error sending confirmation email: {e}")
-        
-        return redirect('signin')
-        
-    return render(request, "authentication/signup.html")
-
-
-def activate(request,uidb64,token):
-    try:
-        uid = force_str(urlsafe_base64_decode(uidb64))
-        myuser = User.objects.get(pk=uid)
-    except (TypeError,ValueError,OverflowError,User.DoesNotExist):
-        myuser = None
-
-    if myuser is not None and generate_token.check_token(myuser,token):
-        myuser.is_active = True
-        # user.profile.signup_confirmation = True
-        myuser.save()
-        login(request,myuser)
-        messages.success(request, "Your Account has been activated!!")
-        return redirect('signin')
-    else:
-        return render(request,'activation_failed.html')
-
-
-def signin(request):
+@csrf_exempt
+def create_chat(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        pass1 = request.POST['pass1']
-        
-        user = authenticate(username=username, password=pass1)
-        
-        if user is not None:
-            login(request, user)
-            fname = user.first_name
-            # messages.success(request, "Logged In Sucessfully!!")
-            return render(request, "authentication/index.html",{"fname":fname})
-        else:
-            messages.error(request, "Bad Credentials!!")
-            return redirect('home')
-    
-    return render(request, "authentication/signin.html")
+        text = request.POST.get('text', '')
+        if text:
+            chat = Chat.objects.create(user=request.user, title=text[:50])
+            Message.objects.create(chat=chat, user=request.user, text=text)
+
+            bot, created  = User.objects.get_or_create(email="bot@bot.com", password="botforwhat")
+            
+            # Simulate a bot response        
+            bot_response = get_chatbot_response(text)
+            Message.objects.create(chat=chat, user= bot, text=bot_response)
+            
+            return JsonResponse({'status': 'success', 'chat_id': chat.uid, 'response': bot_response})
+        return JsonResponse({'status': 'error', 'message': 'No text provided'})
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
+
+# @csrf_exempt
+# def create_chat(request):
+#     if request.method == 'POST':
+#         text = request.POST.get('text', '')
+#         if text:
+#             chat = Chat.objects.create(user=request.user, title=text[:30])
+#             Message.objects.create(user=request.user, chat=chat, text=text)
+#             return JsonResponse({'status': 'success', 'chat_id': chat.uid, 'title': text[:30]})
+#     return JsonResponse({'status': 'error', 'message': 'Invalid request'})
+
+# @csrf_exempt
+# def add_message(request, chat_id):
+#     if request.method == 'POST':
+#         text = request.POST.get('text', '')
+#         if text:
+#             chat = get_object_or_404(Chat, uid=chat_id)
+#             Message.objects.create(user=request.user, chat=chat, text=text)
+#             if greet(text):
+#                 return JsonResponse({'response': greet(text)})
+#             else:
+#                 sentence_tokens.append(text)
+#                 word_tokens.extend(nltk.word_tokenize(text))
+#                 bot_response = response(text)
+#                 sentence_tokens.remove(text)
+#             return JsonResponse({'status': 'success', 'response': bot_response})
+#     return JsonResponse({'status': 'error', 'message': 'Invalid request'})
 
 
-def signout(request):
-    logout(request)
-    messages.success(request, "Logged Out Successfully!!")
-    return redirect('home')
+@csrf_exempt
+def add_message(request, chat_id):
+    if request.method == 'POST':
+        chat = get_object_or_404(Chat, uid=chat_id)
+        text = request.POST.get('text', '')
+        if text:
+            Message.objects.create(chat=chat, user=request.user, text=text)
+
+            # Simulate a bot response
+            bot_response = get_chatbot_response(text)
+            bot, created  = User.objects.get_or_create(email="bot@bot.com", password="botforwhat")
+            
+            # Save bot response to the database
+            Message.objects.create(chat=chat, user=bot, text=bot_response)
+            
+            return JsonResponse({'status': 'success', 'response': bot_response})
+        return JsonResponse({'status': 'error', 'message': 'No text provided'})
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
+
+
+def chat_view(request, chat_id):
+    chats = Chat.objects.filter(user=request.user).order_by('-created')
+    chat = get_object_or_404(Chat, uid=chat_id)
+    messages = Message.objects.filter(chat=chat).order_by('-created')
+    return render(request, 'chat/index.html', {'chats': chats, 'chat': chat, 'messages': messages})
